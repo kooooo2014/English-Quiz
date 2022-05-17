adfb0c6acd143ee3d739b6dc7969bac351d3ffe3
kooooo2014 <20141763kooooo@gmail.com>
2022-05-17 11:25:41 +0900

    create Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <img src="../assets/image1.png" alt="">
+      <div class="right-contents">
+        <p>英語の発音の違い<br>を学習する</p>
+        <a href="/test" class="blue-button">Let's start!</a>
+      </div>
+    </div>  
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style scoped>
+.home {
+    width: 100%;
+    height: 100vh;
+    background: #ffb3001c;
+}
+
+.container {
+    display: flex;
+    justify-content: space-around;
+}
+
+img {
+    width: 500px;
+    height: 500px;
+
+}
+
+.right-contents {
+    margin: auto 100px auto 0;
+    text-align: center;
+}
+
+p {
+    font-family: 'Gothic A1';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 50px;
+}
+
+.blue-button {
+    display: inline-block;
+    margin: auto;
+    height: 70px;
+    width: 280px;
+    border-radius: 20px;
+    background: #97C3FF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    font-family: 'Alef';
+    font-size: 24px;
+    line-height: 70px;
+    color: #fff;
+
+    
+}
+.blue-button :hover {
+    background: #71acff;
+}
+    
+
+
+</style>
